7e90fe25959902d9dc3c2cb0f4635bf8ac2217a6
Oscar Bernal <oscarbernal@Oscars-MacBook-Pro.local>
2019-09-10 16:10:24 -0500

    created the small script with some messages from internet

---
+++ index.js
@@ -0,0 +1,10 @@
+const msgs = require('./messages');
+limit = Math.floor(Math.random() * (7) + 3);
+print = (msg,wait) => {setTimeout(() => process.stdout.write(msg),wait)}
+sync = 100
+for( i = limit ; limit > 0 ; limit-- ){
+    pos = Math.floor(Math.random()*msgs.length)
+    m = msgs[pos]+'\n'
+    m.split(/([\.,])/g).map((val,idx) => {print(val,sync);sync+=idx*100})
+    sync += Math.floor(Math.random()*700+200)
+}
