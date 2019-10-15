726fa8cfa5474c2cee09fb586c3b6d62aa25a56e
Oscar Bernal <oscar.bernal@lever.co>
2019-10-15 08:47:56 -0500

    Changes the wait times, and updates on the messages

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const msgs = require('./messages');
-limit = Math.floor(Math.random() * (7) + 3);
+limit = Math.floor(Math.random() * (5) + 1);
 print = (msg,wait) => {setTimeout(() => process.stdout.write(msg),wait)}
 sync = 100
 for( i = limit ; limit > 0 ; limit-- ){
     pos = Math.floor(Math.random()*msgs.length)
     m = msgs[pos]+'\n'
     m.split(/([\.,])/g).map((val,idx) => {print(val,sync);sync+=idx*100})
-    sync += Math.floor(Math.random()*700+200)
+    sync += Math.floor(Math.random()*500+200)
 }
